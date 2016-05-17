dc442a984c0d9ad051634986184c891fecf69361
krack3 <mike@krackeonline.com>
2016-05-17 10:46:27 -0500

    Single Post Bug Fix
    single-post.php overrides the ability to switch posts based on
    categories, so single-post.php has been moved to single-default.php and
    now this theme can switch based on certain categories

--- single-default.php
+++ single-default.php
@@ -1,21 +1,33 @@
-<?php get_header(); 
+<?php get_header(); ?>
 
-
-
-if (have_posts()) :
-	while (have_posts()) : the_post(); ?>
-<?php endwhile; endif; ?>
-<div class="page-container">
-  <div class="full-page-content">
-
+<section class="page-container">
+<div class="display-inline-block">
+  <aside class="background-green-light">
+    <div class=" fixed-position">
+      <?php dynamic_sidebar('community-sidebar'); ?>
+      <?php dynamic_sidebar( 'newsletter_sidebar' ); ?>
+    </div>
+  </aside>
+  <main>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <div class="page-feature-img">
+      <?php if ( has_post_thumbnail() ) {
+	the_post_thumbnail( 'feature-image' ); } ?>
+    </div>
     <h1>
       <?php the_title(); ?>
     </h1>
-
-    <hr />
-    
-    <?php the_content(); ?>
-  </div>
-
+    <p>
+      <?php the_content(); ?>
+    </p>
+    <div class="comment-container">
+      <?php comments_template(); ?>
+    </div>
+    <?php endwhile; else : ?>
+    <p>
+      <?php _e( 'Sorry, There is nothing to display. '); ?>
+    </p>
+    <?php endif; ?>
+  </main>
 </div>
 <?php get_footer(); ?>
