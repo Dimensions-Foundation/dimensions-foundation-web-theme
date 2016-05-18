4c2dac414c17e90e1e5a2499a7384c7ceeab0988
krack3 <mike@krackeonline.com>
2016-05-18 10:18:16 -0500

    Sidebar CSS/Certified Pages
    Made changes to standardize all sidebar menus.
    Added more to certified classroom pages

--- single-default.php
+++ single-default.php
@@ -1,33 +1,32 @@
-<?php get_header(); ?>
+
 
 <section class="page-container">
-<div class="display-inline-block">
-  <aside class="background-green-light">
-    <div class=" fixed-position">
-      <?php dynamic_sidebar('community-sidebar'); ?>
-      <?php dynamic_sidebar( 'newsletter_sidebar' ); ?>
-    </div>
-  </aside>
-  <main>
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <div class="page-feature-img">
-      <?php if ( has_post_thumbnail() ) {
-	the_post_thumbnail( 'feature-image' ); } ?>
-    </div>
-    <h1>
-      <?php the_title(); ?>
-    </h1>
-    <p>
-      <?php the_content(); ?>
-    </p>
-    <div class="comment-container">
-      <?php comments_template(); ?>
-    </div>
-    <?php endwhile; else : ?>
-    <p>
-      <?php _e( 'Sorry, There is nothing to display. '); ?>
-    </p>
-    <?php endif; ?>
-  </main>
-</div>
-<?php get_footer(); ?>
+	<div class="display-inline-block">
+		<aside class="background-green-light">
+			<div class=" fixed-position">
+				<?php dynamic_sidebar('community-sidebar'); ?>
+				<?php dynamic_sidebar( 'newsletter_sidebar' ); ?>
+			</div>
+		</aside>
+		<main>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<div class="page-feature-img">
+					<?php if ( has_post_thumbnail() ) {
+						the_post_thumbnail( 'feature-image' ); } ?>
+					</div>
+					<h1>
+						<?php the_title(); ?>
+					</h1>
+					<p>
+						<?php the_content(); ?>
+					</p>
+					<div class="comment-container">
+						<?php comments_template(); ?>
+					</div>
+				<?php endwhile; else : ?>
+					<p>
+						<?php _e( 'Sorry, There is nothing to display. '); ?>
+					</p>
+				<?php endif; ?>
+			</main>
+		</div>
--- single-recertified.php
+++ single-recertified.php
@@ -1,23 +1,34 @@
-<?php get_header();
 
-/* Template Name:  Recertified Classroom Post Template */
-
-if (have_posts()) :
-	while (have_posts()) : the_post(); ?>
-
-<div class="container" >
-  <div class="sidebar">
-    <div class="widget-bar" >
-      <?php dynamic_sidebar( 'custom_sidebar' ); ?>
-    </div>
-  </div>
-  <div class="main-content">
-    <?php the_title('<h1 class="classroom-title">', '</h1>');
-	the_excerpt();
-if ( has_post_thumbnail() ) {
-	the_post_thumbnail( 'feature-image' );}
-?>
-    <?php endwhile; endif; the_content(); ?>
-  </div>
-</div>
-<?php get_footer(); ?>
+<section class="page-container">
+	<div class="display-inline-block">
+		<aside class="background-green-light">
+			<div class=" fixed-position">
+				<?php $children = get_pages('child_of='.get_post_top_ancestor_id()); ?>
+				<?php if( count( $children ) != 0 ) { ?>
+					<ul class="sidebar-nav">
+						<span class="sidebar-nav-title" >
+							<?php wp_list_pages( array('title_li'=>'','include'=>get_post_top_ancestor_id()) ); ?>
+						</span>
+						<?php wp_list_pages( array('title_li'=>'','depth'=>1,'child_of'=>get_post_top_ancestor_id()) ); ?>
+					</ul>
+					<?php } ?>
+					<?php dynamic_sidebar( 'custom_sidebar' ); ?>
+					<?php dynamic_sidebar( 'newsletter_sidebar' ); ?>
+				</div>
+			</aside>
+			<main>
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<?php the_title('<h1 class="classroom-title">', '</h1>');
+					the_excerpt();
+					if ( has_post_thumbnail() ) {
+						the_post_thumbnail();}
+						?>
+						<?php the_content(); ?>
+					</p>
+				<?php endwhile; else : ?>
+					<p>
+						<?php _e( 'Sorry, There is nothing to display. '); ?>
+					</p>
+				<?php endif; ?>
+			</main>
+		</div>
