b1083d8b67272a1bc97aee2814900880e588ee4e
krack3 <mike@krackeonline.com>
2016-06-01 13:28:21 -0500

    Attachment CSS

--- attachment.php
+++ attachment.php
@@ -1,23 +1,25 @@
 <?php get_header(); ?>
 
 <section class="page-container background-white">
-<div class="full-page-container center-content">
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-  <?php if (wp_attachment_is_image($post->id)) {
-      $att_image = wp_get_attachment_image_src( $post->id, "medium");
-      ?>
-  <p class="attachment"> <a href="<?php echo wp_get_attachment_url($post->id); ?>" title="<?php the_title(); ?>"> <img src="<?php echo $att_image[0];?>" width="<?php echo $att_image[1];?>" height="<?php echo $att_image[2];?>"  class="attachment-medium" alt="<?php $post->post_excerpt; ?>" /> </a> </p>
-  <?php } ?>
-  <div class="attachment-content">
-    <h1 class="attachment-title">
-      <?php the_title(); ?>
-    </h1>
-    <?php the_content(); ?>
-  </div>
-  <?php endwhile; else : ?>
-  <p>
-    <?php _e( 'Sorry, There is nothing to display. '); ?>
-  </p>
-  <?php endif; ?>
-</div>
-<?php get_footer(); ?>
+  <div id="full-page-container">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php if (wp_attachment_is_image($post->id)) {
+        $att_image = wp_get_attachment_image_src( $post->id, "medium");
+        ?>
+        <span class="attachment">
+          <img src="<?php echo $att_image[0];?>"
+          width="<?php echo $att_image[1];?>"
+          height="<?php echo $att_image[2];?>"
+          class="attachment-medium"
+          alt="<?php $post->post_excerpt; ?>" />
+        </span>
+        <?php } ?>
+        <div class="attachment-content">
+          <h1 class="attachment-title">
+            <?php the_title(); ?>
+          </h1>
+          <?php the_content(); ?>
+        </div>
+      <?php endwhile; endif; ?>
+    </div>
+    <?php get_footer(); ?>
