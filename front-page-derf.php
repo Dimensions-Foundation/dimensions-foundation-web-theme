a15e79ffa6ac2cfc76418a66f5b51982db45af00
krack3 <mike@krackeonline.com>
2016-06-02 11:39:50 -0500

    DERF Edits
    added DERF changes

--- front-page-derf.php
+++ front-page-derf.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <section class="page-container background-white">
-<div class="full-page-container derf-homepage">
+<div id="full-page-container" class="derf-homepage">
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
   <?php the_content(); ?>
   <?php endwhile;  endif; ?>
