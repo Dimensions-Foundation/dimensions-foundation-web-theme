e1bc860f27e618112d7c1f07c190a5769fee8eef
krack3 <mike@krackeonline.com>
2016-06-20 14:53:48 -0500

    NEP CSS

--- header.php
+++ header.php
@@ -1,59 +1,63 @@
 <?php $blog_id = get_current_blog_id();
- 	  $blog_url = get_home_url();
- 	  if ( ( $blog_id != 1 ) &&
-		  ( !is_user_logged_in() ) &&
-		  ( !is_front_page() ) ) {
-			header( 'Location: ' . $blog_url  );
-	}
-	$blog_url = get_home_url();
-		$site_logo_class = '';
-		$site_primary_menu_class = 'horizontal-menu';
+$blog_url = get_home_url();
+if ( ( $blog_id != 1 ) &&
+( !is_user_logged_in() ) &&
+( !is_front_page() ) ) {
+  header( 'Location: ' . $blog_url  );
+}
+$blog_url = get_home_url();
+$site_logo_class = '';
+$site_top_container_class = 'background-blue';
+$site_primary_menu_class = 'horizontal-menu';
 
-		 if ( strstr( $blog_url, 'dimensionsfoundation.org' ) ) {
-			 $site_logo_class = 'derf-logo';
-			 $site_primary_menu_class .= " derf-primary-menu";
-		 } else  if ( strstr( $blog_url, 'dimensionsed.org' ) ) {
-			 $site_logo_class = 'dep-logo';
-			 $site_primary_menu_class .= " dep-primary-menu";
-		 } else  if ( strstr( $blog_url, 'natureexplore.org' ) ) {
-			 $site_logo_class = 'nep-logo';
-			 $site_primary_menu_class .= " nep-primary-menu";
-		 }
+if ( strstr( $blog_url, 'dimensionsfoundation.org' ) ) {
+  $site_logo_class = 'derf-logo';
+  $site_top_container_class .= ' derf-top-menu';
+  $site_primary_menu_class .= " derf-primary-menu";
+} else  if ( strstr( $blog_url, 'dimensionsed.org' ) ) {
+  $site_logo_class = 'dep-logo';
+  $site_top_container_class .= ' dep-top-menu';
+  $site_primary_menu_class .= " dep-primary-menu";
+} else  if ( strstr( $blog_url, 'natureexplore.org' ) ) {
+  $site_logo_class = 'nep-logo';
+  $site_top_container_class .= ' nep-top-menu';
+  $site_primary_menu_class .= " nep-primary-menu";
+}
 
-	?>
+?>
 
 
 <!doctype html>
 <html>
 <head>
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>
-<?php wp_title(); ?>
-</title>
-<?php wp_head(); ?>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>
+    <?php wp_title(); ?>
+  </title>
+  <?php wp_head(); ?>
 </head>
 <?php $defaults = array(
-		'theme_location' 		=> 'top-menu-location',
-		'container'				=> 'nav',
-  'container_class' => 'background-blue',
+  'theme_location' 		=> 'top-menu-location',
+  'container'				=> 'nav',
+  'container_class' =>$site_top_container_class ,
   'container_id' => 'top-menu-container',
-		'menu_id' 				=> 'top-menu',
-		'menu_class'				=> 'horizontal-menu line-break ',
-	);
+  'menu_id' 				=> 'top-menu',
+  'menu_class'				=> 	'horizontal-menu line-break',
+);
 
-	wp_nav_menu( $defaults );	?>
+wp_nav_menu( $defaults );	?>
 <header>
   <div class="<?php echo 'site-logo ' . $site_logo_class; ?>"><a href='<?php echo get_site_url( 1 ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img src='<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'></a></div>
   <?php $defaults = array(
-			'theme_location' 		=> 'primary-menu-location',
-			'container'				=> 'nav',
-'container_id' => 'primary-menu-container',
-			'menu_id' 				=> 'primary-menu',
-			'menu_class'				=> $site_primary_menu_class,
-		);
+    'theme_location' 		=> 'primary-menu-location',
+    'container'				=> 'nav',
+    'container_id' => 'primary-menu-container',
+    'menu_id' 				=> 'primary-menu',
+    'menu_class'				=> $site_primary_menu_class,
+  );
 
-		wp_nav_menu( $defaults );		?>
+  wp_nav_menu( $defaults );		?>
 
 </header>
 
