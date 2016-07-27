3cbd7c255ccc873786da6d007fae1de452533070
krack3 <mike@krackeonline.com>
2016-07-27 15:53:09 -0500

    Updated Blog/Version Change
    Updated index.php to assist with the blog posts

--- page.php
+++ page.php
@@ -1,9 +1,14 @@
 <?php get_header(); ?>
 
 
-<div id="full-page-container">
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-  <?php the_content(); ?>
-  <?php endwhile;endif; ?>
-</div>
-<?php get_footer(); ?>
+    <main>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+          <?php the_content(); ?>
+        <?php endwhile; else : ?>
+          <p>
+            <?php _e( 'Sorry, There is nothing to display. '); ?>
+          </p>
+        <?php endif; ?>
+      </main>
+      <?php get_footer(); ?>
